9e80c51954e037a020dc0900518c6227515205b9
lunatiq-san <lunatiq86@gmail.com>
2021-09-27 11:36:46 +0300

    Refactor by folder

---
+++ schemas/contact.js
@@ -0,0 +1,14 @@
+const Joi = require("joi");
+
+const joiSchema = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+  phone: Joi.string().min(7).max(14).required(),
+});
+
+module.exports = joiSchema;
