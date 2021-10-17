5db8791ef833c1b9e204570b70520c1f371923c1
lunatiq-san <lunatiq86@gmail.com>
2021-10-17 22:53:13 +0300

    pagination

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,9 +1,11 @@
 const contactsOperations = require("../../services/contacts");
 
 const getAll = async (req, res, next) => {
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
   const { _id } = req.user;
 
-  const contacts = await contactsOperations.listContacts(_id);
+  const contacts = await contactsOperations.listContacts(_id, skip, limit);
   res.json({
     status: "success",
     code: 200,
--- services/contacts/listContacts.js
+++ services/contacts/listContacts.js
@@ -1,7 +1,10 @@
 const { Contact } = require("../../models");
 
-const listContacts = async (owner) => {
-  return await Contact.find({ owner }).populate("owner", "email");
+const listContacts = async (owner, skip, limit) => {
+  return await Contact.find({ owner }, "_id name email phone owner", {
+    skip,
+    limit: Number(limit),
+  }).populate("owner", "email");
 };
 
 module.exports = listContacts;
